2f04c85925343aeb0d63190431bd462695a33b4f
Tavan Thiry <tavanthiry@Arilds-Mac-mini.local>
2025-08-12 16:03:44 +0200

    Added user to body

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -24,7 +24,7 @@ const MessageForm = ({ onAddMessage, disabled, user }) => {
         'Content-Type': 'application/json',
         'Authorization': `${token}`
        },
-      body: JSON.stringify({ message: message.trim() })
+      body: JSON.stringify({ user, message: message.trim() })
     })
       .then(res => res.json())
       .then(data => {
